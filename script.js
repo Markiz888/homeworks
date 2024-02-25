e524fc4cf18dbedf233465c026907dd5c781c8f8
Markiz888 <markiz888n@gmail.com>
2024-02-25 16:29:28 +0100

    hm_14_02_24

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(
+  function () {
+    console.log('Document ready!');
+  }
+)
+
+$("#addHtmlt").append("<h1> New Element </h1>");
+
+$(".second").hide();
+
+$("section").remove();
+
+$("div").addClass('addClass');
+$("div").text('Lumos Maxima');
