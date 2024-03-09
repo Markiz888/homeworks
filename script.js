d5dff7f3ef6d9673d5bcf423477284d96e67c847
Markiz888 <markiz888n@gmail.com>
2024-03-09 14:54:37 +0100

    reworked

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let start = document.querySelector("#on");
 let finisf = document.querySelector("#off");
+let timerDate
 function myTimer() {
+    timerDate = new Date()
     alert(`Время размяться, вы работаете ${time} минут`);
   }
 function work_time(){
   let current = new Date();
   let sec = Math.floor((current.getTime() - timerDate.getTime())/1000);
   let date = new Date(null);
-  console.log(current, date);
   date.setSeconds(sec);
   var timeString = date.toISOString().substr(11, 8);
   currentTime.innerText = timeString
 }
 let time = 0
-let a, b, current, timerDate
+let a, b, current
 const currentTime = document.querySelector("#current-time");
 if (start) {
   start.addEventListener("click", () => {
